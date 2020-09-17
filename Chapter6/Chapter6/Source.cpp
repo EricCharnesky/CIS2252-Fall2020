9e175a95c8c613f472a4791c4d00f48ff5e91523
Eric Charnesky <eric.charnesky@careevolution.com>
2020-09-16 20:04:38 -0400

    more function examples

--- Chapter6/Chapter6/Source.cpp
+++ Chapter6/Chapter6/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -6,13 +7,17 @@ void printHelloInLotsOfLanguages();
 int calculateMagicNumber(int number);
 int askUserForANumber(string message);
 double calculateAverage(int first, int second, int third);
+bool isFirstLargerThanSecond(int first, int second);
+void squareTheValue(int& number);
+string createDateString(int day, int month, int year);
 
 int main()
 {
 	printHelloInLotsOfLanguages();
 
-	cout << calculateMagicNumber(askUserForANumber(
-		"Enter a number between 1 - 10")) << endl;
+	int number = askUserForANumber("Enter a number between 1 - 10");
+	cout << calculateMagicNumber(number) << endl;
+	cout << "Your number was: " << number << endl;
 
 	cout << "Pick another one, I bet it adds up to 9..." << endl;
 
@@ -21,6 +26,15 @@ int main()
 
 	cout << "The average of 51, 70, 24 is: " << 
 		calculateAverage(51, 70, 24) << endl;
+
+	int two = 2;
+	for (int timesToBeSquared = 0; timesToBeSquared < 16; timesToBeSquared++)
+	{
+		cout << two << endl;
+		squareTheValue(two);
+	}
+
+	cout << two << endl;
 
 	return 0;
 }
@@ -57,7 +71,52 @@ int askUserForANumber(string message)
 	return number;
 }
 
-double calculateAverage(int first, int second, int third)
+// this is a bad example of a default, third might actually be 0
+double calculateAverage(int first, int second, int third = 0)
 {
+	double numberOfValues = 2;
+	if (third != 0)
+	{
+		numberOfValues++;
+	}
 	return (first + second + third) / 3.0;
+}
+
+bool isFirstLargerThanSecond(int first, int second)
+{
+	if (first > second)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+	// eric's pet peeve - this is the same thing!
+	return first > second;
+}
+
+void squareTheValue(int& number)
+{
+	number *= number;
+}
+
+// please don't do this in real life - this is only an example
+string createDateString(int day, int month, int year, char format = 'U' )
+{
+	// found to_string on https://www.educba.com/c-plus-plus-int-to-string/
+	if (format == 'U') // US format
+	{
+		return to_string(month) + "/" + to_string(day) + "/" + to_string(year);
+	}
+	else if (format == 'E')
+	{
+		return to_string(day) + "/" + to_string(month) + "/" + to_string(year);
+	}
+	else if ( format == 'I')
+	{
+		return to_string(year) + "-" + to_string(month) + "-" + to_string(day);
+	}
+	
+	
 }
