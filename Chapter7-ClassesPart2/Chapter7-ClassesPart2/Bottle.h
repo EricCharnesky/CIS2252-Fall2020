f55bec732028c1028d3b459db38f589a8743cc47
Eric Charnesky <eric.charnesky@careevolution.com>
2020-10-07 18:59:54 -0400

    Classes part 2 - getting started

---
+++ Chapter7-ClassesPart2/Chapter7-ClassesPart2/Bottle.h
@@ -0,0 +1,61 @@
+#pragma once
+
+class Bottle
+{
+public:
+	void setCapacityInMililiters(int capacityInMililiters)
+	{
+		if (capacityInMililiters < 0)
+		{
+			this->capacityInMililiters = 0;
+		}
+		else
+		{
+			this->capacityInMililiters = capacityInMililiters;
+		}
+	}
+	int getCapacityInMililiters()
+	{
+		return capacityInMililiters;
+	}
+
+	int getCurrentVolumeOfSomethingInMililiters()
+	{
+		return currentVolumeInMililiters;
+	}
+
+	bool addMililitersOfSomething(int mililitersToAdd)
+	{
+		currentVolumeInMililiters += mililitersToAdd;
+		
+		if (currentVolumeInMililiters > capacityInMililiters)
+		{
+			currentVolumeInMililiters = capacityInMililiters;
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+	bool removeMililitersOfSOmething(int mililitersToRemove)
+	{
+		currentVolumeInMililiters -= mililitersToRemove;
+		
+		if (currentVolumeInMililiters < 0)
+		{
+			currentVolumeInMililiters = 0;
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+private:
+	int capacityInMililiters;
+	int currentVolumeInMililiters;
+
+};
---
+++ Chapter7-ClassesPart2/UnitTest1/UnitTest1.cpp
@@ -0,0 +1,47 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\Chapter7-ClassesPart2\Bottle.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethodCapacity)
+		{
+			// AAA - Arrange, Act, Assert
+			
+			// Arrange
+			Bottle bottle;
+			int expectedCapacity = 355;
+
+			// Act
+			bottle.setCapacityInMililiters(expectedCapacity);
+			int actualCapacity = bottle.getCapacityInMililiters();
+
+			// Assert
+			Assert::AreEqual(expectedCapacity, actualCapacity);
+		}
+
+		TEST_METHOD(TestMethodVolume)
+		{
+			// AAA - Arrange, Act, Assert
+
+			// Arrange
+			Bottle bottle;
+			int expectedCapacity = 355;
+			int expectedVolume = 300;
+
+			// Act
+			bottle.setCapacityInMililiters(expectedCapacity);
+			bottle.addMililitersOfSomething(expectedVolume);
+			int actualVolume = bottle.getCurrentVolumeOfSomethingInMililiters();
+
+			// Assert
+			Assert::AreEqual(expectedVolume, actualVolume);
+		}
+	};
+}
